7ab53bf33ff83838217bc89e273593276b8132ad
Thomas <thomas.bennett3@my.jcu.edu.au>
2019-08-20 09:33:02 +1000

    Ass password check program

--- Password_entry.py
+++ Password_entry.py
@@ -1,1 +1,10 @@
 """"well hello there obi wan kenobi, we meet at last. i am the great Thomas Bennett"""
+password = input("enter a password:")
+
+while len(password) < 10:
+    print("password is not long enough")
+    password = input("re-enter a password:")
+
+for i in range(len(password)):
+    print('*', end='')
+
